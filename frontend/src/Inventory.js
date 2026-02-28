695e9b3c22f8df619a932645079e9a8c77c70963
sanaya-bhardwaj <sanayabhardwaj3@gmail.com>
2026-02-28 18:55:51 +0530

    Fix Vercel CI build issue

--- frontend/src/Inventory.js
+++ frontend/src/Inventory.js
@@ -11,7 +11,7 @@ function Inventory({ setActiveTab }) {
   }, []);
 
   const fetchMedicines = async () => {
-    const res = await axios.get("http://127.0.0.1:8000/medicines");
+    const res = await axios.get(`${API}/medicines`);
     setMedicines(res.data);
   };
 
